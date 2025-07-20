dd83cb05fe284bd6862c8e0b6a26813f0f646da8
aleksejbelkin1017 <aleksejbelkin1017@gmail.com>
2025-07-21 00:54:59 +0300

    Added MEDIA_URL, MEDIA_ROOT in settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT
 from dotenv import load_dotenv
 
 # Загрузка переменных из .env файла
@@ -7,9 +9,9 @@ load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True if os.getenv('DEBUG') == 'True' else False
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -88,6 +90,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
