56895774096c294a1d8c408bafdadf11f3ffe9c4
aleksejbelkin1017 <aleksejbelkin1017@gmail.com>
2025-07-27 18:04:55 +0300

    Added picture to products

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, products_list, contacts
+from catalog.views import home, products_list, contacts, products_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", products_list, name="product_list"),
+    path("", products_list, name="products_list"),
+    path("products/<int:pk>/", products_detail, name="products_detail"),
     path("home/", home, name="home"),
     path("contacts/", contacts, name="contacts"),
 ]
