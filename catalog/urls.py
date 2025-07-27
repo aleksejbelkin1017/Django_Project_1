0d171c5fcd0c43f8ffe8f81e285bb6d69a6a243b
aleksejbelkin1017 <aleksejbelkin1017@gmail.com>
2025-07-27 15:03:44 +0300

    Files 'urls.py' and 'views.py' have been updated.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, index, contacts
+from catalog.views import home, products_list, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", index),
+    path("", products_list, name="product_list"),
     path("home/", home, name="home"),
     path("contacts/", contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
-def index(request):
-    return render(request, "base.html")
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products_list.html", context)
 
 
 def home(request):
