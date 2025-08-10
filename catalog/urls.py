2a1335d373b8a13d104c7a4ba02efc1be577e991
aleksejbelkin1017 <aleksejbelkin1017@gmail.com>
2025-08-10 17:55:19 +0300

    update views.py and urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ContactsView, ProductDetailView
+from catalog.views import ProductListView, ContactsView, ProductDetailView, ProductCreateView
+
+from . import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="products_list"),
+    path("products/create/", views.ProductCreateView.as_view(), name="product_create"),
+    path("products/update/<int:pk>/", views.ProductUpdateView.as_view(), name="product_update"),
+    path("products/delete/<int:pk>/", views.ProductDeleteView.as_view(), name="product_delete"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="products_detail"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_create.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_update.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+    def get_success_url(self):
+        # Перенаправление на страницу просмотра статьи
+        return reverse('catalog:products_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ProductListView(ListView):
